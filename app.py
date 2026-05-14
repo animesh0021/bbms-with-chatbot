49d67397a4e821a242062983d733d240d36ccf6f
Animesh Agarkar <agarkaranimesh@gmail.com>
2026-05-14 20:19:52 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -579,4 +579,6 @@ if __name__ == '__main__':
             db.session.commit()
             print("Default admin user created!")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # --- MODIFIED PORT BINDING FOR RENDER ---
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
